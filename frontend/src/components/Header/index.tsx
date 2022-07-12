934a875fcf15a88085af36b929d0c40b334709f0
fernando-angeli <fernando_angeli@hotmail.com>
2022-07-12 10:41:27 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+import logo from '../../assets/img/logo.svg'
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="Logotipo da página." />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/fernando-angeli/">@fernando-angeli</a>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
