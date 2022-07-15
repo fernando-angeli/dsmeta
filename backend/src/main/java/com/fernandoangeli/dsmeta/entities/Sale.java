59f266137f77b17f70f114feb8622eb05597878d
fernando-angeli <fernando_angeli@hotmail.com>
2022-07-15 16:35:48 -0300

    Database

---
+++ backend/src/main/java/com/fernandoangeli/dsmeta/entities/Sale.java
@@ -0,0 +1,26 @@
+package com.fernandoangeli.dsmeta.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_sales")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private Date date;
+
+}
